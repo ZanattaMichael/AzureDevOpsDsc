a1bd5363a05fe19d9c537016e74f90d8437733d3
Michael Zanatta <zanattaam@gmail.com>
2025-07-02 15:21:54 +1000

    Daily Commit.

---
+++ tests/Unit/Modules/AzureDevOpsDsc.Common/Resources/Functions/Public/AzDoAreaPermission/Set-AzDoAreaPermission.tests.ps1
@@ -0,0 +1,32 @@
+$currentFile = $MyInvocation.MyCommand.Path
+
+Describe 'Set-AzDoAreaPermission' {
+
+
+    AfterAll {
+        Remove-Variable -Name DSCAZDO_OrganizationName -Scope Global
+    }
+
+    BeforeAll {
+
+        $Global:DSCAZDO_OrganizationName = 'TestOrganization'
+
+        # Load the functions to test
+        if ($null -eq $currentFile) {
+            $currentFile = Join-Path -Path $PSScriptRoot -ChildPath 'Set-AzDoAreaPermission.tests.ps1'
+        }
+
+        # Load the functions to test
+        $files = Get-FunctionItem (Find-MockedFunctions -TestFilePath $currentFile)
+
+        ForEach ($file in $files) {
+            . $file.FullName
+        }
+        # Load Get-AzDoCacheObjects
+        . (Get-FunctionItem 'Get-AzDoCacheObjects.ps1')
+
+        # Load the summary state
+        . (Get-ClassFilePath 'DSCGetSummaryState')
+        . (Get-ClassFilePath '000.CacheItem')
+        . (Get-ClassFilePath 'Ensure')
+        . (Get-ClassFilePath '002.LocalizedDataAzSerializationPatten')
