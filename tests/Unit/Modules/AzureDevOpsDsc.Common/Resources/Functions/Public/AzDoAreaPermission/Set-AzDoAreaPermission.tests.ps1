598b153ae608e6507ea7dc969aa9bd0447a9d033
Michael Zanatta <zanattaam@gmail.com>
2025-07-03 08:01:16 +1000

    Finished Initial Unit Tests

--- tests/Unit/Modules/AzureDevOpsDsc.Common/Resources/Functions/Public/AzDoAreaPermission/Set-AzDoAreaPermission.tests.ps1
+++ tests/Unit/Modules/AzureDevOpsDsc.Common/Resources/Functions/Public/AzDoAreaPermission/Set-AzDoAreaPermission.tests.ps1
@@ -30,3 +30,93 @@ Describe 'Set-AzDoAreaPermission' {
         . (Get-ClassFilePath '000.CacheItem')
         . (Get-ClassFilePath 'Ensure')
         . (Get-ClassFilePath '002.LocalizedDataAzSerializationPatten')
+
+        Mock -CommandName Get-CacheItem {
+            param ([string]$Key, [string]$Type)
+            if ($Key -eq 'CSS' -and $Type -eq 'SecurityNamespaces') {
+                return @{ namespaceId = "12345" }
+            } elseif ($Key -eq $ProjectName -and $Type -eq 'LiveProjects') {
+                return @{ Name = "SampleProject" }
+            } elseif ($Key -eq "12345" -and $Type -eq 'LiveACLList') {
+                return @(
+                    @{ token = "vstfs:///Classification/Node/1" },
+                    @{ token = "vstfs:///Classification/Node/2" }
+                )
+            }
+        }
+
+        Mock -CommandName ConvertTo-ACLHashtable {
+            return @{ SerializedACLs = "SerializedData" }
+        }
+
+        Mock -CommandName Set-AzDoPermission
+
+    }
+
+    Context "When AreaPath is not specified" {
+        It "Should write a warning and stop execution" {
+            Mock -CommandName Write-Warning -Verifiable
+            { Set-AzDoAreaPermission -ProjectName "TestProject" -isInherited $true } |
+                Should -Not -Throw
+        }
+    }
+
+    Context "When Security Namespace is not found" {
+
+        It "Should write an error message" {
+
+            Mock -CommandName Get-CacheItem {
+                return $null
+            }
+
+            Mock Write-Error -Verifiable
+            { Set-AzDoAreaPermission -ProjectName "TestProject" -AreaPath "SampleAreaPath" -isInherited $true } |
+                Should -Not -Throw
+        }
+    }
+
+    Context "When Project is not found" {
+
+        It "Should write an error message" {
+
+            Mock -CommandName Get-CacheItem {
+                param ([string]$Key, [string]$Type)
+                if ($Key -eq 'CSS' -and $Type -eq 'SecurityNamespaces') {
+                    return @{ namespaceId = "12345" }
+                } else {
+                    return $null
+                }
+            }
+
+            Mock Write-Error -Verifiable
+
+            { Set-AzDoAreaPermission -ProjectName "TestProject" -AreaPath "SampleAreaPath" -isInherited $true } |
+                Should -Not -Throw
+        }
+    }
+
+    Context "When AreaPath is not specified" {
+        It "Should set ClearACEs to true and call Set-AzDoPermission" {
+            $LookupResult = @{
+                identifiers = @("1")
+                propertiesChanged = @{}
+                DifferenceACLs = @{}
+            }
+            Set-AzDoAreaPermission -ProjectName "SampleProject" -isInherited $true -LookupResult $LookupResult
+
+            Assert-MockCalled -CommandName Set-AzDoPermission -Exactly 1
+        }
+    }
+
+    Context "When AreaPath is specified" {
+        It "Should not set ClearACEs and call Set-AzDoPermission" {
+            $LookupResult = @{
+                identifiers = @("1")
+                propertiesChanged = @{}
+            }
+            Set-AzDoAreaPermission -ProjectName "SampleProject" -AreaPath "SampleAreaPath" -isInherited $true -LookupResult $LookupResult
+
+            Assert-MockCalled -CommandName Set-AzDoPermission -Exactly 1
+        }
+    }
+}
